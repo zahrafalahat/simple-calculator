38f0474ff33c798e815110def26b108058fb9080
zahra falahat <falahathasti8@gmail.com>
2026-06-26 12:16:17 -0700

    add simple calculator project

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+print('welcome to simple calcutalor')
+num1 = int(input('enter first number'))
+num2 = input('enter operator')
+num3 = int(input('enter second number'))
+
+if num2 == '+':
+    print(num1 + num3)
+elif num2 == '-':
+    print(num1 - num3)  
+elif num2 == '*':
+    print(num1 * num3)
+elif num2 == '/':
+    print(num1 / num3)
+else:
+    print('invalid operator ')
 
